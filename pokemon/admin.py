ee7c29b31ab75e8ef5a6d405e4e91cc5d293f17a
athouba99 <athooba99@gmail.com>
2022-10-13 05:48:22 +0300

    finished admin task

--- pokemon/admin.py
+++ pokemon/admin.py
@@ -9,3 +9,9 @@ class PokemonAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "hp", "active",)
     list_display_links = ("id", "name",)
     list_filter = ("active",)
+
+    fieldsets = (
+        ("general", {"fields": ("name", "hp", "active", "type",)}),
+        ("localization", {"fields": ("name_fr", "name_ar", "name_jp")}),
+        None, {"fields": ("created_at", "updated_at",)})
+    # fieldssets has str and dictionary
